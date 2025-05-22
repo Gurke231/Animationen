b44b9a9a94157e3ee63eb534faba69c7915107b7
Müller <niklas.mueller>
2025-05-22 11:22:42 +0200

    vor dings

--- Pixelsalat in the Dark/player.gd
+++ Pixelsalat in the Dark/player.gd
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	if health <= 0:
 		player_alive = false
 		print("player ist tod")
-		#self.queue_free()
+		self.queue_free()
 
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
